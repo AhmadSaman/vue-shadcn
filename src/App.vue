5a2e6c19dace0730bd00278f92b32c1b51590260
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2025-03-14 10:50:53 +0300

    config: Shadcn UI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from "./components/ui/button";
+</script>
 
 <template>
   <div>
     <h1 class="text-4xl text-center text-green-300">Hello Vue</h1>
+
+    <Button>Click me</Button>
   </div>
 </template>
 
