0bc08f36553e32f11bfbdfa7f7493e94e53da60d
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2025-03-14 20:30:54 +0300

    hotfix: product type

--- src/general.ts
+++ src/general.ts
@@ -14,9 +14,7 @@ export type Product = {
   price: number;
   description: string;
   category: Category;
-  images: [
-    "https://click.gt/cdn/shop/files/Sintitulo-1_3538b668-0b95-4a62-9e07-8a3f3d94a363.png?v=1733181609&width=600",
-  ];
+  images: string[];
   creationAt: string;
   updatedAt: string;
 };
