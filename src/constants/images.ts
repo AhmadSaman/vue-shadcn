5c7875222588c1fc69fcec2466b6c32d448b968c
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2025-03-15 06:20:06 +0300

    add: static images

---
+++ src/constants/images.ts
@@ -0,0 +1,59 @@
+import clothesOne from "@/assets/clothes-1.webp";
+import clothesTwo from "@/assets/clothes-2.webp";
+import clothesThree from "@/assets/clothes-3.webp";
+import clothesFour from "@/assets/clothes-4.webp";
+import toastMachine from "@/assets/toast.webp";
+import chair from "@/assets/chair.webp";
+import headset from "@/assets/headset.webp";
+import furniture from "@/assets/furniture.webp";
+
+export const STATIC_IMAGES = [
+  {
+    src: clothesOne,
+    name: "Clothes One",
+    class:
+      "absolute bottom-3 right-1/3 hidden w-[100px] -rotate-3 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: clothesTwo,
+    name: "Clothes Two",
+    class:
+      "absolute left-20 top-8 hidden w-[100px] -rotate-12 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: clothesThree,
+    name: "Clothes Three",
+    class:
+      "absolute right-20 top-8 hidden w-[100px] rotate-12 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: clothesFour,
+    name: "Clothes Four",
+    class:
+      "absolute bottom-8 left-20 hidden w-[100px] rotate-6 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: toastMachine,
+    name: "Toast Machine",
+    class:
+      "absolute bottom-3 left-1/3 hidden w-[100px] rotate-6 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: chair,
+    name: "Chair",
+    class:
+      "absolute right-1/3 top-3 hidden w-[100px] rotate-12 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: headset,
+    name: "Headset",
+    class:
+      "absolute bottom-8 right-20 hidden w-[100px] -rotate-6 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+  {
+    src: furniture,
+    name: "Furniture",
+    class:
+      "absolute left-1/3 top-3 hidden w-[100px] rotate-3 rounded-3xl opacity-70 shadow-md duration-1000 animate-in md:block",
+  },
+];
