9719c661bd3a7d1f5b980926f4d2109d7e325e78
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2025-03-15 06:22:30 +0300

    feat: implement home page

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -5,10 +5,13 @@ import Badge from "./ui/badge/Badge.vue";
 import { Star } from "lucide-vue-next";
 import { useFavoritesStore } from "@/stores/favorites";
 import type { Product } from "@/general";
+import { useVisitedProductsStore } from "@/stores/visited";
 
 const props = defineProps<{ product: Product }>();
 const { product } = toRefs(props);
+
 const favoritesStore = useFavoritesStore();
+const visitedStore = useVisitedProductsStore();
 
 const handleFavorite = (product: Product) => {
   if (favoritesStore.isFavorite(product.id)) {
@@ -16,6 +19,14 @@ const handleFavorite = (product: Product) => {
   } else {
     favoritesStore.addToFavorites(product);
   }
+};
+
+const checkIfVisited = (productId: number) => {
+  return visitedStore.isVisited(productId);
+};
+
+const recordProductVisit = (product: Product) => {
+  visitedStore.addToVisited(product);
 };
 </script>
 
@@ -34,17 +45,31 @@ const handleFavorite = (product: Product) => {
         :class="favoritesStore.isFavorite(product.id) ? 'fill-neutral-900' : ''"
       />
     </Button>
-    <img :src="product.images[0]" alt="" srcset="" class="rounded-3xl p-4" />
-    <div class="flex flex-col gap-1 text-center">
-      <p class="line-clamp-1">
-        {{ product.title }}
-      </p>
-      <p class="text-2xl font-medium">{{ product.price }}$</p>
-      <div>
-        <Badge class="rounded-3xl px-2 py-1" variant="secondary">{{
-          product.category.name
-        }}</Badge>
+    <Button
+      class="absolute -left-2 -top-2 z-10 h-10 rounded-3xl px-4 text-xs shadow transition-all duration-200 hover:scale-105 hover:bg-neutral-100"
+      variant="default"
+      size="xs"
+      v-if="checkIfVisited(product.id)"
+      @click="recordProductVisit(product)"
+    >
+      Visited
+    </Button>
+    <RouterLink
+      @click="recordProductVisit(product)"
+      :to="`product/${product.slug}`"
+    >
+      <img :src="product.images[0]" alt="" srcset="" class="rounded-3xl p-4" />
+      <div class="flex flex-col gap-1 text-center">
+        <p class="line-clamp-1">
+          {{ product.title }}
+        </p>
+        <p class="text-2xl font-medium">{{ product.price }}$</p>
+        <div>
+          <Badge class="rounded-3xl px-2 py-1" variant="secondary">{{
+            product.category.name
+          }}</Badge>
+        </div>
       </div>
-    </div>
+    </RouterLink>
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,81 @@
 <script setup lang="ts">
-import { Button } from "@/components/ui/button";
-import { useCounterStore } from "@/stores/counter";
+import { STATIC_IMAGES } from "@/constants/images";
+import { Input } from "@/components/ui/input";
+import { Loader2, Search } from "lucide-vue-next";
+import { onMounted, ref, watch } from "vue";
+import { getProducts } from "@/api/products";
+import type { Product } from "@/general";
+import ProductCard from "@/components/ProductCard.vue";
 
-const counterStore = useCounterStore();
+const products = ref<Product[]>([]);
+const isProductLoading = ref(false);
+const searchQuery = ref("");
+const debounceTimeout = ref<number | null>(null);
+
+const fetchProducts = async (search?: string) => {
+  isProductLoading.value = true;
+  try {
+    const { data } = await getProducts({ search });
+    products.value = data as Product[];
+    isProductLoading.value = false;
+  } catch (error) {
+    console.error("Failed to fetch products:", error);
+    isProductLoading.value = false;
+  }
+};
+
+const handleSearch = (value: string) => {
+  if (debounceTimeout.value) {
+    clearTimeout(debounceTimeout.value);
+  }
+
+  debounceTimeout.value = setTimeout(() => {
+    fetchProducts(value);
+  }, 300);
+};
+
+watch(searchQuery, (newValue) => {
+  handleSearch(newValue);
+});
+
+onMounted(() => {
+  fetchProducts();
+});
 </script>
 
 <template>
-  <div class="custom-container mx-auto">
-    <h1>Home Page</h1>
-    <p>Welcome to the Home Page</p>
-    <div>
-      <p>{{ counterStore.name }}</p>
-      <p>{{ counterStore.count }}</p>
-      <Button @click="counterStore.increment">Increment</Button>
-    </div>
-  </div>
+  <main class="custom-container mx-auto my-5 flex flex-col gap-8">
+    <section class="relative flex items-center justify-center md:h-96">
+      <div v-for="image in STATIC_IMAGES" :key="image.name">
+        <img :src="image.src" :class="image.class" :alt="image.name" />
+      </div>
+      <div class="relative w-full items-center md:max-w-md">
+        <Input
+          id="search"
+          type="text"
+          v-model="searchQuery"
+          placeholder="Search..."
+          class="rounded-3xl bg-white p-6 pl-12 transition-all duration-200"
+        />
+        <span
+          class="absolute inset-y-0 start-2 flex items-center justify-center px-2"
+        >
+          <Search class="text-muted-foreground size-6" />
+        </span>
+      </div>
+    </section>
+    <section class="flex-1">
+      <div class="flex w-full flex-wrap justify-center gap-6 self-center">
+        <div v-if="isProductLoading" class="text-center">
+          <Loader2 class="animate-spin" />
+        </div>
+        <ProductCard
+          v-else
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
+      </div>
+    </section>
+  </main>
 </template>
